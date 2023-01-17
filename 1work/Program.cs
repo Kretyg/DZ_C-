3cc5c8b0561f5482943252aa3069572408299b88
Kretyg <sokolovsky.zhora58@gmail.com>
2023-01-17 22:41:30 +0300

    add 1 work

---
+++ 1work/Program.cs
@@ -0,0 +1,22 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+
+System.Console.WriteLine("Введите число дня которое вам нужно");
+int numDayOfWeek = Convert.ToInt32(Console.ReadLine());
+string[] dayOfWeek = {"Понедельник","Вторник","Среда","Четверг","Пятница","Суббота","Воскресенье"};
+switch(numDayOfWeek){
+    case 1:    
+         Console.WriteLine(dayOfWeek[0]); break;
+    case 2:    
+         Console.WriteLine(dayOfWeek[1]); break;
+     case 3:    
+         Console.WriteLine(dayOfWeek[2]); break;
+     case 4:    
+         Console.WriteLine(dayOfWeek[3]); break;
+     case 5:    
+         Console.WriteLine(dayOfWeek[4]); break;
+     case 6:    
+         Console.WriteLine("Это выходной день " + dayOfWeek[5]); break;
+     case 7:    
+         Console.WriteLine("Это выходной день " + dayOfWeek[6]); break;
+     
+}
