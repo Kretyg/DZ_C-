740f2ed20aa7299778e596645076bec19523d59b
Kretyg <sokolovsky.zhora58@gmail.com>
2023-01-26 00:09:02 +0300

    Доделал задание

--- 2work/Program.cs
+++ 2work/Program.cs
@@ -1,5 +1,19 @@
 ﻿//По двум заданным числам проверять является ли одно квадратом другого
 
-int number1 = int.Parse(Console.ReadLine()!);
-int number2 = int.Parse(Console.ReadLine()!);
-if(number1 )
+Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+
+if (b * b == a)
+{
+    Console.WriteLine("Первое число являеться корнем второго");
+}
+else if (a * a == b)
+{
+    Console.WriteLine("Второе число являеться корнем первого");
+}
+else
+{
+    Console.WriteLine("Числа не являются корнями друг друга");
+}
