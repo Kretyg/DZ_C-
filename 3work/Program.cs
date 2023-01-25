8864654914aef92cd6118737372570a7465c4dc0
Kretyg <sokolovsky.zhora58@gmail.com>
2023-01-26 00:09:25 +0300

    Доделал задание

---
+++ 3work/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+
+Console.WriteLine("Введите 1 из 4 четвертей: ");
+int quarterNum = int.Parse(Console.ReadLine()!);
+
+if (quarterNum == 1)
+{
+    Console.WriteLine("x > 0, y > 0");
+}
+else if (quarterNum == 2) 
+{ 
+    Console.WriteLine("x < 0, y > 0");
+}
+else if (quarterNum == 3) 
+{
+    Console.WriteLine("x < 0, y < 0");
+}
+else 
+{
+    Console.WriteLine("x > 0, y < 0");
+}
